7f15ce1b78b1e6a4702a0151d9988764cb29d9a4
Maryam Ashfaq <01-135201-037@student.bahria.edu.pk>
2024-11-24 06:21:08 +0500

    readmore...

--- Script.js
+++ Script.js
@@ -126,3 +126,17 @@ const observer = new IntersectionObserver(
 ); // Adjust threshold as needed
 
 observer.observe(document.getElementById("AboutMe"));
+
+document.addEventListener('DOMContentLoaded', function() {
+  const readMoreLinks = document.querySelectorAll('.read-more');
+  
+  readMoreLinks.forEach(link => {
+      link.addEventListener('click', function() {
+          const hiddenContent = this.nextElementSibling;
+          if (hiddenContent) {
+              hiddenContent.style.display = 'block';
+              this.style.display = 'none';
+          }
+      });
+  });
+});
